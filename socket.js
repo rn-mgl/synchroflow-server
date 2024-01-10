e863a49da375c65c36feea07de9a0b50fff64c4e
rn-mgl <reinb2ra@gmail.com>
2024-01-10 20:37:14 +0800

    real time on associate invites

--- socket.js
+++ socket.js
@@ -1,24 +1,30 @@
-const socketEmits = (socket) => {
+export const sockets = (socket) => {
   socket.emit("room_rejoin");
-};
 
-const socketOns = (socket) => {
   socket.on("connect_to_uuid", (args) => {
     socket.join(args?.uuid);
+  });
+
+  socket.on("rejoin_user_uuid", (args) => {
+    if (args.room) {
+      socket.join(args.room);
+      console.log(socket.rooms);
+    }
   });
 
   socket.on("associate_invite", (args) => {
     socket.to(args.room).emit("update_associate_invite");
   });
 
-  socket.on("rejoin_user_uuid", (args) => {
-    if (args.room) {
-      socket.join(args.room);
-    }
+  socket.on("disconnect_associate", (args) => {
+    socket.to(args.room).emit("update_associates");
   });
-};
 
-export const sockets = (socket) => {
-  socketOns(socket);
-  socketEmits(socket);
+  socket.on("remove_associate_invite", (args) => {
+    socket.to(args.room).emit("remove_associate_invite", { args });
+  });
+
+  socket.on("accept_associate_invite", (args) => {
+    socket.to(args.room).emit("update_associates");
+  });
 };
