9910b6a24aead86339203dac91181e7ab25d3436
rn-mgl <reinb2ra@gmail.com>
2024-01-10 19:13:49 +0800

    can use web socket in associate invite

--- socket.js
+++ socket.js
@@ -1,17 +1,24 @@
 const socketEmits = (socket) => {
-  socket.emit("hello", { id: socket.id });
+  socket.emit("room_rejoin");
 };
 
 const socketOns = (socket) => {
-  socket.on("connect to uuid", (args) => {
+  socket.on("connect_to_uuid", (args) => {
     socket.join(args?.uuid);
+  });
+
+  socket.on("associate_invite", (args) => {
+    socket.to(args.room).emit("update_associate_invite");
+  });
+
+  socket.on("rejoin_user_uuid", (args) => {
+    if (args.room) {
+      socket.join(args.room);
+    }
   });
 };
 
 export const sockets = (socket) => {
-  console.log(socket.id);
-
-  socketEmits(socket);
-
   socketOns(socket);
+  socketEmits(socket);
 };
