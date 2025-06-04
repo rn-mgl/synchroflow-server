59f8695222a30e86152e9335af947b0c56bec918
rn-mgl <reinb2ra@gmail.com>
2025-06-04 19:56:17 +0800

    fix checking

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,12 +1,24 @@
 import { StatusCodes } from "http-status-codes";
 import jwt from "jsonwebtoken";
 import { v4 as uuidv4 } from "uuid";
-import { BadRequestError, NotFoundError, UnauthorizedError } from "../errors/index.js";
+import {
+  BadRequestError,
+  NotFoundError,
+  UnauthorizedError,
+} from "../errors/index.js";
 import { UserSettings } from "../models/UserSettings.js";
 import { Users } from "../models/Users.js";
-import { comparePassword, createLoginToken, hashPassword, randomAvatar } from "../utils/index.js";
+import {
+  comparePassword,
+  createLoginToken,
+  hashPassword,
+  randomAvatar,
+} from "../utils/index.js";
 import { createEmailToken } from "../utils/tokenUtils.js";
-import { sendPasswordResetMail, sendVerificationMail } from "./mailController.js";
+import {
+  sendPasswordResetMail,
+  sendVerificationMail,
+} from "./mailController.js";
 
 export const registerUser = async (req, res) => {
   const { registerCredentials } = req.body;
@@ -21,7 +33,9 @@ export const registerUser = async (req, res) => {
   const newUser = await user.createUser();
 
   if (!newUser) {
-    throw new BadRequestError("Error in creating your account. Try again later.");
+    throw new BadRequestError(
+      "Error in creating your account. Try again later."
+    );
   }
 
   const userSettingsUUID = uuidv4();
@@ -31,10 +45,17 @@ export const registerUser = async (req, res) => {
   const createUserSettings = await userSettings.createUserSettings();
 
   if (!createUserSettings) {
-    throw new BadRequestError("Error in initializing your settings. Try again later.");
+    throw new BadRequestError(
+      "Error in initializing your settings. Try again later."
+    );
   }
 
-  const token = createEmailToken(newUser.insertId, userUUID, `${name} ${surname}`, email);
+  const token = createEmailToken(
+    newUser.insertId,
+    userUUID,
+    `${name} ${surname}`,
+    email
+  );
 
   res.status(StatusCodes.OK).json(token);
 
@@ -47,8 +68,10 @@ export const loginUser = async (req, res) => {
 
   const userEmail = await Users.getUser(["email"], [candidateEmail]);
 
-  if (!userEmail) {
-    throw new BadRequestError("There is no account found with the given email.");
+  if (!userEmail || !userEmail[0]) {
+    throw new BadRequestError(
+      "There is no account found with the given email."
+    );
   }
 
   const { user_id, user_uuid, name, surname, email, password } = userEmail[0];
@@ -59,9 +82,20 @@ export const loginUser = async (req, res) => {
     throw new UnauthorizedError(`The email and password does not match.`);
   }
 
-  const token = createLoginToken(user_id, user_uuid, `${name} ${surname}`, email);
+  const token = createLoginToken(
+    user_id,
+    user_uuid,
+    `${name} ${surname}`,
+    email
+  );
 
-  const primary = { id: user_id, uuid: user_uuid, name: `${name} ${surname}`, email, token: `Bearer ${token}` };
+  const primary = {
+    id: user_id,
+    uuid: user_uuid,
+    name: `${name} ${surname}`,
+    email,
+    token: `Bearer ${token}`,
+  };
 
   res.status(StatusCodes.OK).json(primary);
 };
@@ -88,7 +122,9 @@ export const verifyUser = async (req, res) => {
   const verifyUser = await Users.updateUserVerification(user[0]?.user_id);
 
   if (!verifyUser) {
-    throw new BadRequestError(`Error in verifying the account. Try again later.`);
+    throw new BadRequestError(
+      `Error in verifying the account. Try again later.`
+    );
   }
 
   res.status(StatusCodes.OK).json(verify);
@@ -97,7 +133,10 @@ export const verifyUser = async (req, res) => {
 export const forgotPassword = async (req, res) => {
   const { candidateName, candidateSurname, candidateEmail } = req.body;
 
-  const user = await Users.getUser(["name", "surname", "email"], [candidateName, candidateSurname, candidateEmail]);
+  const user = await Users.getUser(
+    ["name", "surname", "email"],
+    [candidateName, candidateSurname, candidateEmail]
+  );
 
   if (!user[0]) {
     throw new NotFoundError(`There is no user with the given identifiers.`);
@@ -105,9 +144,18 @@ export const forgotPassword = async (req, res) => {
 
   const { user_id, user_uuid, name, surname, email } = user[0];
 
-  const token = createEmailToken(user_id, user_uuid, `${name} ${surname}`, email);
+  const token = createEmailToken(
+    user_id,
+    user_uuid,
+    `${name} ${surname}`,
+    email
+  );
 
-  const passwordResetMail = await sendPasswordResetMail(`${name} ${surname}`, email, token);
+  const passwordResetMail = await sendPasswordResetMail(
+    `${name} ${surname}`,
+    email,
+    token
+  );
 
   res.status(StatusCodes.OK).json({ token, passwordResetMail });
 };
@@ -131,7 +179,9 @@ export const newPassword = async (req, res) => {
   const newUserPassword = await Users.updateUserPassword(hashedPassword, id);
 
   if (!newUserPassword) {
-    throw new BadRequestError(`Error in changing your password. Try again later.`);
+    throw new BadRequestError(
+      `Error in changing your password. Try again later.`
+    );
   }
 
   res.status(StatusCodes.OK).json(newUserPassword);
