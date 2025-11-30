21afe367b4499f933127f15239c9d1930e6f4ca1
rn-mgl <reinb2ra@gmail.com>
2025-11-30 13:54:45 +0800

    package upgrade

--- utils/tokenUtils.js
+++ utils/tokenUtils.js
@@ -1,11 +1,17 @@
 import jwt from "jsonwebtoken";
 
 export const createLoginToken = (id, uuid, name, email) => {
-  const token = jwt.sign({ id, uuid, name, email }, process.env.JWT_SECRET, { expiresIn: process.env.JWT_TTL });
+  console.log(process.env.JWT_TTL);
+  const token = jwt.sign({ id, uuid, name, email }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_TTL,
+  });
   return token;
 };
 
 export const createEmailToken = (id, uuid, name, email) => {
-  const token = jwt.sign({ id, uuid, name, email }, process.env.JWT_SECRET, { expiresIn: process.env.EMAIL_TTL });
+  console.log(process.env.EMAIL_TTL);
+  const token = jwt.sign({ id, uuid, name, email }, process.env.JWT_SECRET, {
+    expiresIn: process.env.EMAIL_TTL,
+  });
   return token;
 };
