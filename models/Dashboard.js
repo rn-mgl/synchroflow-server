089ec7b3fb27556b86f95920d2980031ccf98d1d
rn-mgl <reinb2ra@gmail.com>
2026-01-17 14:39:30 +0800

    fixes

--- models/Dashboard.js
+++ models/Dashboard.js
@@ -7,67 +7,80 @@ export class Dashboard {
   static async getDashboardData(userID) {
     try {
       const tasks = `SELECT 
-                    COUNT(DISTINCT CASE WHEN mt.main_task_by = ? 
-                          AND mt.main_task_status = 'ongoing'
-                          THEN mt.main_task_id END) 
-                          +
-                    COUNT(DISTINCT CASE WHEN mt.main_task_by <> ? 
-                          AND mtc.collaborator_fk_id = ?
-                          AND mt.main_task_status = 'ongoing'
-                          THEN mt.main_task_id END)
-                          AS ongoingMainTasksCount,
+                  -- Ongoing Main Tasks (owned or collaborated)
+                  (SELECT COUNT(DISTINCT mt.main_task_id)
+                  FROM main_tasks AS mt
+                  WHERE mt.main_task_by = ? 
+                  AND mt.main_task_status = 'ongoing')
+                  +
+                  (SELECT COUNT(DISTINCT mt.main_task_id)
+                  FROM main_tasks AS mt
+                  INNER JOIN main_task_collaborators AS mtc 
+                        ON mt.main_task_id = mtc.main_task_fk_id
+                  WHERE mt.main_task_by <> ? 
+                  AND mtc.collaborator_fk_id = ?
+                  AND mt.main_task_status = 'ongoing')
+                  AS ongoingMainTasksCount,
 
-                    COUNT(DISTINCT CASE WHEN mt.main_task_by = ? 
-                          AND mt.main_task_status = 'done' 
-                          THEN mt.main_task_id END)
-                          +
-                    COUNT(DISTINCT CASE WHEN mt.main_task_by <> ? 
-                          AND mtc.collaborator_fk_id = ?
-                          AND mt.main_task_status = 'done'
-                          THEN mt.main_task_id END) 
-                          AS doneMainTasksCount,
+                  -- Done Main Tasks (owned or collaborated)
+                  (SELECT COUNT(DISTINCT mt.main_task_id)
+                  FROM main_tasks AS mt
+                  WHERE mt.main_task_by = ? 
+                  AND mt.main_task_status = 'done')
+                  +
+                  (SELECT COUNT(DISTINCT mt.main_task_id)
+                  FROM main_tasks AS mt
+                  INNER JOIN main_task_collaborators AS mtc 
+                        ON mt.main_task_id = mtc.main_task_fk_id
+                  WHERE mt.main_task_by <> ? 
+                  AND mtc.collaborator_fk_id = ?
+                  AND mt.main_task_status = 'done')
+                  AS doneMainTasksCount,
 
-                    COUNT(DISTINCT CASE WHEN mt.main_task_by = ? 
-                          AND mt.main_task_status = 'late' 
-                          THEN mt.main_task_id END)
-                          +
-                    COUNT(DISTINCT CASE WHEN mt.main_task_by <> ? 
-                          AND mtc.collaborator_fk_id = ?
-                          AND mt.main_task_status = 'late'
-                          THEN mt.main_task_id END) 
-                          AS lateMainTasksCount,
+                  -- Late Main Tasks (owned or collaborated)
+                  (SELECT COUNT(DISTINCT mt.main_task_id)
+                  FROM main_tasks AS mt
+                  WHERE mt.main_task_by = ? 
+                  AND mt.main_task_status = 'late')
+                  +
+                  (SELECT COUNT(DISTINCT mt.main_task_id)
+                  FROM main_tasks AS mt
+                  INNER JOIN main_task_collaborators AS mtc 
+                        ON mt.main_task_id = mtc.main_task_fk_id
+                  WHERE mt.main_task_by <> ? 
+                  AND mtc.collaborator_fk_id = ?
+                  AND mt.main_task_status = 'late')
+                  AS lateMainTasksCount,
 
-                    COUNT(DISTINCT CASE WHEN (st.sub_task_id IS NOT NULL
-                          AND st.sub_task_by <> ? 
-                          AND stc.collaborator_fk_id = ? 
-                          AND st.sub_task_status = 'ongoing') 
-                          THEN st.sub_task_id END) AS ongoingSubTasksCount,
+                  -- Ongoing Sub Tasks (collaborated only)
+                  (SELECT COUNT(DISTINCT st.sub_task_id)
+                  FROM sub_tasks AS st
+                  INNER JOIN sub_task_collaborators AS stc 
+                        ON st.sub_task_id = stc.sub_task_fk_id
+                  WHERE st.sub_task_by <> ? 
+                  AND stc.collaborator_fk_id = ?
+                  AND st.sub_task_status = 'ongoing')
+                  AS ongoingSubTasksCount,
 
-                    COUNT(DISTINCT CASE WHEN (st.sub_task_id IS NOT NULL
-                          AND st.sub_task_by <> ? 
-                          AND stc.collaborator_fk_id = ? 
-                          AND st.sub_task_status = 'done') 
-                          THEN st.sub_task_id END) AS doneSubTasksCount,
+                  -- Done Sub Tasks (collaborated only)
+                  (SELECT COUNT(DISTINCT st.sub_task_id)
+                  FROM sub_tasks AS st
+                  INNER JOIN sub_task_collaborators AS stc 
+                        ON st.sub_task_id = stc.sub_task_fk_id
+                  WHERE st.sub_task_by <> ? 
+                  AND stc.collaborator_fk_id = ?
+                  AND st.sub_task_status = 'done')
+                  AS doneSubTasksCount,
 
-                    COUNT(DISTINCT CASE WHEN (st.sub_task_id IS NOT NULL
-                          AND st.sub_task_by <> ? 
-                          AND stc.collaborator_fk_id = ? 
-                          AND st.sub_task_status = 'late') 
-                          THEN st.sub_task_id END) AS lateSubTasksCount
-                    
-                    FROM users AS u
-
-                    LEFT JOIN main_tasks AS mt 
-                    ON u.user_id = mt.main_task_by
-
-                    LEFT JOIN main_task_collaborators AS mtc 
-                    ON mt.main_task_id = mtc.main_task_fk_id
-
-                    LEFT JOIN sub_tasks AS st 
-                    ON mt.main_task_id = st.main_task_fk_id
-
-                    LEFT JOIN sub_task_collaborators AS stc 
-                    ON st.sub_task_id = stc.sub_task_fk_id`;
+                  -- Late Sub Tasks (collaborated only)
+                  (SELECT COUNT(DISTINCT st.sub_task_id)
+                  FROM sub_tasks AS st
+                  INNER JOIN sub_task_collaborators AS stc 
+                        ON st.sub_task_id = stc.sub_task_fk_id
+                  WHERE st.sub_task_by <> ? 
+                  AND stc.collaborator_fk_id = ?
+                  AND st.sub_task_status = 'late')
+                  AS lateSubTasksCount`;
 
       const weeklyTasks = `SELECT DAYOFWEEK(mt.main_task_end_date) AS day,
                         COUNT(mt.main_task_id) AS taskCount
@@ -125,7 +138,7 @@ export class Dashboard {
       const [tasksCount, _tasks] = await conn.execute(tasks, tasksValues);
       const [weekTasksCount, _weeklyTasks] = await conn.execute(
         weeklyTasks,
-        weeklyTaskValues
+        weeklyTaskValues,
       );
 
       return { tasksCount: tasksCount[0], weekTasksCount };
